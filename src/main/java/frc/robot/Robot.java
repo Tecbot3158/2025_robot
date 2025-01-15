98389df4d06b2048527a3982cd93cf76378e2ee2
Joe Mondragon <jose.c.mondragon@gmail.com>
2025-01-14 18:30:44 -0600

    added libs
    fixed libs.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,14 @@
 
 package frc.robot;
 
+import edu.wpi.first.hal.CANAPIJNI;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  
 
   private final RobotContainer m_robotContainer;
 
